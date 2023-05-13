883d3eed52482f581ce28601a9ee92fc2b3382cd
Vishnutharan <karunakaran.vishnu6@gmail.com>
2023-05-13 14:31:58 +0530

    Angular Basic Practice

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Export like public
+export class AppComponent {
+  // here we set the variable 
+  title = 'my-app';
+  name ='vishnu';
+}
